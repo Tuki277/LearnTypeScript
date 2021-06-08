380120fafcd0d7fb54f6fa5ffbbca23b1d7c2d0c
Tran Hoang Huy <huybeos2707@gmail.com>
2021-06-08 14:36:53 +0700

    add

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // generics <string>
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var last = function (arr) { return arr[arr.length - 1]; }; // chi truyen duoc number
 var l1 = last([1, 2, 3]);
 console.log({ l1: l1 });
@@ -19,3 +30,58 @@ var m3 = makeTuple('x', 5);
 console.log({ m3: m3 });
 var m4 = makeTuple(null, 3);
 console.log({ m4: m4 });
+// generics extends
+// const makeFullName = obj => ({
+//     ...obj,
+//     fullName: `${obj.firstName} ${obj.fullName}`
+// })
+var makeFullNameConstraint = function (obj) { return (__assign(__assign({}, obj), { fullName: obj.firstName + " " + obj.lastName })); };
+var n1 = makeFullNameConstraint({
+    firstName: 'huy',
+    lastName: 'dev'
+});
+console.log({ n1: n1 });
+var makeFullConstraintWithGenerics = function (obj) { return (__assign(__assign({}, obj), { fullName: obj.firstName + " " + obj.lastName })); };
+var n2 = makeFullConstraintWithGenerics({
+    firstName: 'huy',
+    lastName: 'dev',
+    age: 20
+});
+console.log({ n2: n2 });
+var addNewEmployee = function (employee) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, employee), { uid: uid });
+};
+var n3 = addNewEmployee({ name: 'huy', age: 20 });
+console.log({ n3: n3 });
+var addNewEmployeeT = function (employee) {
+    var uid = Math.floor(Math.random() * 100);
+    return __assign(__assign({}, employee), { uid: uid });
+};
+var n4 = addNewEmployeeT({ name: 'name', age: 20, male: true });
+console.log({ n4: n4 });
+console.log(n4.name);
+var numbers = {
+    uid: 2,
+    name: 'number',
+    data: [2, 1, 3]
+};
+var resourceOne = {
+    uid: 1,
+    name: 'huy',
+    data: 'hello'
+};
+var resourceTwo = {
+    uid: 1,
+    name: 'huy1',
+    data: {
+        name: 'huy2',
+        age: 20
+    }
+};
+console.log({ resourceTwo: resourceTwo });
+var resourceThree = {
+    uid: 1,
+    name: 'huy1',
+    data: ['html', 'css']
+};
--- index.ts
+++ index.ts
@@ -39,6 +39,107 @@ const m3 = makeTuple('x', 5)
 
 console.log({ m3 })
 
-const m4 = makeTuple<null, number>(null, 3)
+const m4 = makeTuple<string | null, number>(null, 3)
 
 console.log({ m4 })
+
+// generics extends
+
+// const makeFullName = obj => ({
+//     ...obj,
+//     fullName: `${obj.firstName} ${obj.fullName}`
+// })
+
+const makeFullNameConstraint = (obj: {
+    firstName: string
+    lastName: string
+}) => ({
+    ...obj,
+    fullName: `${obj.firstName} ${obj.lastName}`
+})
+
+const n1 = makeFullNameConstraint({
+    firstName: 'huy',
+    lastName: 'dev'
+})
+
+console.log({ n1 })
+
+const makeFullConstraintWithGenerics = <T extends {firstName: string, lastName: string}>(
+    obj: T
+) => ({
+    ...obj,
+    fullName: `${obj.firstName} ${obj.lastName}`
+})
+
+const n2 = makeFullConstraintWithGenerics({ 
+    firstName : 'huy',
+    lastName: 'dev',
+    age: 20
+})
+
+console.log({ n2 })
+
+const addNewEmployee = (employee: object) => {
+    const uid = Math.floor(Math.random() * 100)
+    return {
+        ...employee,
+        uid
+    }
+}
+
+const n3 = addNewEmployee({ name: 'huy', age: 20})
+
+console.log({ n3 })
+
+const addNewEmployeeT = <T extends object>(employee: T) => {
+    const uid = Math.floor(Math.random() * 100)
+    return {
+        ...employee,
+        uid
+    }
+}
+
+const n4 = addNewEmployeeT({ name: 'name', age: 20, male: true})
+
+console.log({ n4 })
+console.log(n4.name)
+
+//generics in interface
+
+interface Resource<T> {
+    uid: number,
+    name: string,
+    data: T
+}
+
+type NumberResource = Resource<number[]>
+
+const numbers: NumberResource = {
+    uid: 2,
+    name: 'number',
+    data: [2, 1, 3]
+}
+
+const resourceOne: Resource<string> = {
+    uid: 1,
+    name: 'huy',
+    data: 'hello'
+}
+
+const resourceTwo: Resource<object> = {
+    uid: 1,
+    name: 'huy1',
+    data: {
+        name: 'huy2',
+        age: 20
+    }
+}
+
+console.log({ resourceTwo })
+
+const resourceThree: Resource<string[]> = {
+    uid: 1,
+    name: 'huy1',
+    data: ['html', 'css']
+}
